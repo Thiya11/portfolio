9c1384e0b1a26efebfc2659d2217527636ea59e2
Thiya11 <athiyagu6@gmail.com>
2025-04-30 20:01:34 +0530

    hero section changes to align with updated resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ function Hero() {
                 <div ref={ref} className={`hero-header-section fade-in-section ${isVisible ? 'is-visible' : ''}`}>
                     <h1 className="primary-header">I&rsquo;m Thiyagu, a Software Developer</h1>
                     <p className="secondary-header mb-0">
-                        AI-Enhanced Software Developer with 3+ years of experience, 
+                        AI-Enhanced Software Developer with 2+ years of experience, 
                         proficient in various tech stacks from front-end to back-end.
                     </p>
                     <div className="scroll-circle" onClick={handleScrollClick}>
